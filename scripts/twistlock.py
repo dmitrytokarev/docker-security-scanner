6b17a2c8f527be3895798e220f12974a2d571838
Dustin Van Buskirk <dvanbuskirk@luxoft.com>
2017-11-12 10:54:15 -0800

    debugging updated cli

--- scripts/twistlock.py
+++ scripts/twistlock.py
@@ -78,11 +78,13 @@ def main(argv):
 
   if tl_only == "TRUE": 
     # Run stand-alone Twistlock Scan
-    check_permissions = ['ls -ltra /packages/twistcli']
-    run_check_permissions = subprocess.Popen(check_permissions, shell=True)
-    stdout, stderr = run_check_permissions.communicate()
-    command = ['/packages/twistcli -c "https://' + tl_console_hostname + ':' + tl_console_port + '" -u "' + tl_console_username + '" -p "' + tl_console_password + '" -i ' + docker_image_id + ' --include-files --include-package-files --hash-method sha1']
-    print(command)
+    command = ['/packages/twistcli console help']
+    proc = subprocess.Popen(command, shell=True)
+    stdout, stderr = proc.communicate()
+    command = ['/packages/twistcli images help']
+    proc = subprocess.Popen(command, shell=True)
+    stdout, stderr = proc.communicate()
+    command = ['/packages/twistcli images -c "https://' + tl_console_hostname + ':' + tl_console_port + '" -u "' + tl_console_username + '" -p "' + tl_console_password + '" -i ' + docker_image_id + ' --include-files --include-package-files --hash-method sha1']
     proc = subprocess.Popen(command, shell=True)
     stdout, stderr = proc.communicate()
 
