eeb71e24cf5100826ffb67eb3130dc5fdd620656
Dustin Van Buskirk <dvanbuskirk@luxoft.com>
2017-11-19 19:20:41 -0700

    debugging

--- scripts/twistlock.py
+++ scripts/twistlock.py
@@ -160,7 +160,7 @@ def main(argv):
       tl_report_url = ''.join(re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', stdout))
       with open('/codefresh/volume/env_vars_to_export', 'a') as f:
         print('Setting TL_REPORT_URL to:' + tl_report_url)
-        f.write('export TL_REPORT_URL=' + tl_report_url)
+        f.write('TL_REPORT_URL=' + tl_report_url)
         f.close()
     else:
       proc = subprocess.Popen(twistcli_exec, shell=True)
